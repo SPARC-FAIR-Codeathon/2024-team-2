1d9f3b118438300879b43cb9c82735c840cde3d0
Chinchien Lin <lin810116@gmail.com>
2024-08-12 10:20:34 +1200

    build(setup.py): update authors

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ setup(
     description='A Python tool to find and automatically assemble tools and workflows to process SPARC datasets in accordance with FAIR principles.',
     long_description=(Path(__file__).parent / "README.md").read_text(),
     long_description_content_type="text/markdown",
-    author="Thiranja Prasad Babarenda Gamage, Chinchien Lin, Jiali Xu, Linkun Gao, Mathilde Verlyck, Max Dang Vu",
+    author="Thiranja Prasad Babarenda Gamage, Chinchien Lin, Jiali Xu, Mathilde Verlyck, Max Dang Vu",
     email="psam012@aucklanduni.ac.nz, clin864@aucklanduni.ac.nz",
     license="Apache-2.0",
     packages=find_packages(),
