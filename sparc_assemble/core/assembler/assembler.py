f3827f1a76730bf7b4e21de68ceec3a2ec114a2a
Mathilde <se14mathilde@hotmail.fr>
2024-08-11 14:49:21 +1200

    Create assembler module

---
+++ sparc_assemble/core/assembler/assembler.py
@@ -0,0 +1,11 @@
+class Assembler():
+    """
+    assembler = Assembler(parm1)
+    assembler.execute()
+    """
+
+
+    def __init__(self, source: str, output: str):
+        self.source = source
+    def execute(self):
+        pass
