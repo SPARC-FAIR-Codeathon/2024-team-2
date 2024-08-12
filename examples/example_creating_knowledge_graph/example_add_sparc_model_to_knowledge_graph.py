b9933fc1ec6ad5c8a75af0afd6694e1671d46f14
Chinchien Lin <lin810116@gmail.com>
2024-08-12 22:11:09 +1200

    feat: update add model API - adding sparc model add_sparc_model() - adding biomodels model by add_biomodels_model

--- examples/example_creating_knowledge_graph/example_add_sparc_model_to_knowledge_graph.py
+++ examples/example_creating_knowledge_graph/example_add_sparc_model_to_knowledge_graph.py
@@ -5,9 +5,8 @@ from sparc_assemble.core.kg import KG
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
 
-    # parser.add_argument("--tool_library", type=str, help='Path to tool library', default=r"../../resources/tools")
     parser.add_argument("--model_file", type=str, help='Path to model', default=r"../../resources/models/simulation.json")
-    parser.add_argument("--save_kg_path", type=str, help='Path to save KG', default=r"./kg_model.owl")
+    parser.add_argument("--save_kg_path", type=str, help='Path to save KG', default=r"./kg_sparc_model.owl")
 
     args = parser.parse_args()
 
@@ -19,7 +18,7 @@ if __name__ == '__main__':
     # kg = KG(ontology_file=args.save_kg_path)
 
     # adding tools to KG
-    kg.add_model(model_path=args.model_file)
+    kg.add_sparc_model(model_path=args.model_file)
 
     # listing tools in KG
     kg.list()
