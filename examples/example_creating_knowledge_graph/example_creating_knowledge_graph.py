e70836864e411f6501cf544b70cff967a4cc2d92
Mathilde <se14mathilde@hotmail.fr>
2024-08-13 12:57:11 +1200

    Important change assembler and tutorial 5

--- examples/example_creating_knowledge_graph/example_creating_knowledge_graph.py
+++ examples/example_creating_knowledge_graph/example_creating_knowledge_graph.py
@@ -7,7 +7,6 @@ if __name__ == '__main__':
 
     parser.add_argument("--ontology", type=str, help='Path to ontology template',
                         default=r"../../resources/EDAM.owl")
-    # parser.add_argument("--tool_library", type=str, help='Path to tool library', default=r"../../resources/tools")
     parser.add_argument("--tool_library", type=str, help='Path to tool library', default=r"../../resources/dummy_tools")
     parser.add_argument("--save_kg_path", type=str, help='Path to save KG', default=r"./dummy_kg.owl")
 
